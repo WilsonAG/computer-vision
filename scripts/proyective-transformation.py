c27aa8b995df19c9aca82ba32e6cee6a5e009be3
Wilson Aguilar <waguilars@est.ups.edu.ec>
2020-11-15 14:59:47 -0500

    practices 2 and 3 done

--- scripts/proyective-transformation.py
+++ scripts/proyective-transformation.py
@@ -1,4 +1,44 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_a(po, pd):
+    a = []
+
+    for point in enumerate(po):
+        i = point[0]
+        u, v = po[i]
+        x, y = pd[i]
+
+        a.append([-u, -v, -1, 0, 0, 0, u * x, v * x, x])
+        a.append([0, 0, 0, -u, -v, -1, u * y, v * y, y])
+
+    return np.array(a)
+
+
+def resolve_svd(a_mtx):
+    U, L, Vt = np.linalg.svd(a_mtx)
+    V = np.transpose(Vt)
+    phi = V[:, -1]
+
+    phi = np.reshape(phi, [3, 3])
+
+    return phi
+
+
+def map_point(point, phi_mtx):
+    u, v = point
+    o = np.array([u, v, 1])
+
+    lx, ly, l = phi_mtx.dot(o)
+
+    # print(phi.dot(origin))
+
+    x = int(lx / l)
+    y = int(ly / l)
+
+    return np.array([x, y])
+
 
 origin = np.array([
     [0, 0],
@@ -14,55 +54,30 @@ dest = np.array([
     [0, 200],
 ])
 
+logo = np.load('../datasets/logoUPS.npy')
 
-def getA(origin, dest):
-    A = []
-
-    for point in enumerate(origin):
-        i = point[0]
-        u, v = origin[i]
-        x, y = dest[i]
-
-        A.append([-u, -v, -1, 0, 0, 0, u*x, v*x, x])
-        A.append([0, 0, 0, -u, -v, -1, u*y, v*y, y])
-
-    return np.array(A)
-
-
-def resolve_svd(A):
-    U, L, Vt = np.linalg.svd(A)
-    V = np.transpose(Vt)
-    phi = V[:, -1]
-
-    phi = np.reshape(phi, [3, 3])
-
-    return phi
-
-
-def map_point(point, phi):
-    u, v = point
-    origin = np.array([u, v, 1])
-
-    lx, ly, l = phi.dot(origin)
-
-    # print(phi.dot(origin))
-
-    x = lx/l
-    y = ly/l
-
-    return np.array([x, y])
-
-
-A = getA(origin, dest)
-
-# print(A)
-
+A = get_a(origin, dest)
 phi = resolve_svd(A)
 
-# print(phi)
+y, x = logo.shape
+new_dim = [x, y]
+logo2 = np.zeros(map_point(new_dim, phi))
 
-p1 = [300, 0]
-# p2 = [300, 400]
+for i in range(x):
+    for j in range(y):
+        px, py = map_point([i, j], phi)
+        px = int(np.round(px))
+        py = int(np.round(py))
+        logo2[py, px] = logo[j, i]
 
-print(p1, "-> ", map_point(p1, phi))
-# print(p2, "-> ", map_point(p2, phi))
+
+plt.figure()
+plt.subplot(1, 2, 1)
+plt.imshow(logo)
+plt.subplot(1, 2, 2)
+plt.imshow(logo2)
+# plt.scatter(dest[0, 0], dest[0, 1])
+# plt.scatter(dest[1, 0], dest[1, 1])
+# plt.scatter(dest[2, 0], dest[2, 1])
+# plt.scatter(dest[3, 0], dest[3, 1])
+plt.show()
